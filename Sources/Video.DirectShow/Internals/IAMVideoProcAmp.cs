d53dc5cbaa6473af642c48aac1036fa63e7ccb54
dipwater <dipwater@126.com>
2017-06-19 10:20:27 +0800

    Add files via upload
    add IAMVideoProcAmp.cs file for SetVideoProperty, GetVideoProperty, GetVideoPropertyRange methods.

---
+++ Sources/Video.DirectShow/Internals/IAMVideoProcAmp.cs
@@ -0,0 +1,44 @@
+﻿// AForge Direct Show Library
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2009-2011
+// contacts@aforgenet.com
+//
+
+namespace AForge.Video.DirectShow.Internals
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+    Guid("C6E13360-30AC-11d0-A18C-00A0C9118956"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IAMVideoProcAmp
+    {
+        [PreserveSig]
+        int GetRange(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int pMin,
+            [Out] out int pMax,
+            [Out] out int pSteppingDelta,
+            [Out] out int pDefault,
+            [Out] out VideoProcAmpFlags pCapsFlags
+            );
+
+        [PreserveSig]
+        int Set(
+            [In] VideoProcAmpProperty Property,
+            [In] int lValue,
+            [In] VideoProcAmpFlags Flags
+            );
+
+        [PreserveSig]
+        int Get(
+            [In] VideoProcAmpProperty Property,
+            [Out] out int lValue,
+            [Out] out VideoProcAmpFlags Flags
+            );
+    }
+
+}
